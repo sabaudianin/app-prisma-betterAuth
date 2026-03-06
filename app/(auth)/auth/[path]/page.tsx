8cd102db4eb35ce35fa6ec6442aa82fb6d686e0f
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 <sabaudianin@gmail.com>
2026-03-06 15:57:19 +0100

    add layout and main page auth path

--- app/(auth)/auth/[path]/page.tsx
+++ app/(auth)/auth/[path]/page.tsx
@@ -1,6 +1,6 @@
 import { AuthView } from "@daveyplate/better-auth-ui";
 import { authViewPaths } from "@daveyplate/better-auth-ui/server";
-
+import { Terminal } from "lucide-react";
 export const dynamicParams = false;
 
 export function generateStaticParams() {
@@ -17,8 +17,36 @@ export default async function AuthPage({
     console.log("AuthPage", path)
 
     return (
-        <main className="container flex grow flex-col items-center justify-center self-center p-4 md:p-6">
-            <AuthView path={path} redirectTo="/dashboard" />
-        </main>
+        <div className="w-full max-w-110 p-4">
+            <div className="flex flex-col items-center mb-8 space-y-2">
+                <div className="h-12 w-12 rounded-xl bg-primary/10 border border-primary/20 flex items-center justify-center mb-2 shadow-[0_0_20px_rgba(var(--primary),0.2)]">
+                    <Terminal className="h-6 w-6 animate-pulse font-bold" />
+                </div>
+                <h1 className="text-3xl font-bold tracking-tight text-white font-mono">
+                    Dev<span className="text-secondary">Insight</span>_
+                </h1>
+                <p className="font-semibold text-xs md:text-sm">
+                    Connect your stack. Sync your notes. Build your legacy.
+                </p>
+            </div>
+            <div className="relative group">
+                <div className="absolute -inset-0.5 bg-linear-to-r from-mauve-700 to-taupe-600 rounded-4xl blur group-hover:opacity-40 transition duration-1000"></div>
+                <div className="relative bg-[#0A0A0A]/80 backdrop-blur-xl border border-white/10 p-8 rounded-4xl shadow-2xl">
+                    <AuthView
+                        path={path}
+                        redirectTo="/dashboard"
+                    />
+                    <div className="mt-6 flex flex-col space-y-4">
+                        <div className="relative">
+                            <div className="absolute inset-0 flex items-center"><span className="w-full border-t border-white/5"></span></div>
+                            <div className="relative flex justify-center text-xs uppercase"><span className="px-2 text-white">Developer Access Only</span></div>
+                        </div>
+                        <p className="text-center text-xs text-muted-foreground italic">
+                            By signing in, you agree to  <span className="underline cursor-pointer">Terms of Service</span>.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,12 +4,20 @@ import { Providers } from "@/providers/authUiProvider"
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
         <Providers>
-            <section className="relative min-h-screen flex items-center justify-center p-2 overflow-hidden bg-slate-950">
+            <section className="relative min-h-screen w-full overflow-hidden bg-indigo-200 flex items-center justify-start">
 
-                <div >
+                <div className="absolute inset-0 z-0 opacity-20"
+                    style={{
+                        backgroundImage: `linear-gradient(#333 1px, transparent 1px), linear-gradient(90deg, #333 1px, transparent 1px)`,
+                        backgroundSize: '40px 40px'
+                    }} />
+                <div className="absolute top-0 left-0 w-full h-full bg-linear-to-b from-transparent via-cyan-500/20 to-black/30 z-0" />
+
+                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-125 h-125 bg-primary blur-[120px] rounded-full z-0" />
+
+                <main className="relative z-10 w-full flex flex-col justify-center items-center">
                     {children}
-                </div>
-
+                </main>
             </section>
         </Providers>
     );
